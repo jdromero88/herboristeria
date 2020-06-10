9a66b52a75c000e05aad0848fd5c017abd1aa0d2
jdromero88 <jdromerov88@gmail.com>
2020-06-10 16:35:38 -0400

    Added Header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Navbar from './components/Navbar'
+import Header from './container/Header'
 function App() {
   return (
     <div className="App">
-    <Navbar />
+    <Header />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
