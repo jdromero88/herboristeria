1c399a7e6b45a82443d8abafc4e77ebce571edba
jdromero88 <jdromerov88@gmail.com>
2020-06-10 21:09:18 -0400

    Updated App to show Footer and Content

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Header from './container/Header'
+import Content from './container/Content'
+import Footer from './container/Footer'
+
 function App() {
   return (
     <div className="App">
