19880a1148485959af9c2357e61f53230181d7b9
jdromero88 <jdromerov88@gmail.com>
2020-06-10 21:18:57 -0400

    Added some dummy content to Content

--- src/container/Content.jsx
+++ src/container/Content.jsx
@@ -1,10 +1,120 @@
 import React from 'react'
-
+import { Container, Card, Icon, Image } from 'semantic-ui-react'
 const Content = () => {
   return (
-      <div>
+      <Container>
         <p>El contenido va aqui.</p>
-      </div>
+        <Card.Group>
+          <Card>
+            <Image src='/images/avatar/large/matthew.png' wrapped ui={false} />
+            <Card.Content>
+              <Card.Header>Matthew</Card.Header>
+              <Card.Meta>
+                <span className='date'>Joined in 2015</span>
+              </Card.Meta>
+              <Card.Description>
+                Matthew is a musician living in Nashville.
+              </Card.Description>
+            </Card.Content>
+            <Card.Content extra>
+              <a>
+                <Icon name='user' />
+                22 Friends
+              </a>
+            </Card.Content>
+          </Card>
+          <Card>
+            <Image src='/images/avatar/large/matthew.png' wrapped ui={false} />
+            <Card.Content>
+              <Card.Header>Matthew</Card.Header>
+              <Card.Meta>
+                <span className='date'>Joined in 2015</span>
+              </Card.Meta>
+              <Card.Description>
+                Matthew is a musician living in Nashville.
+              </Card.Description>
+            </Card.Content>
+            <Card.Content extra>
+              <a>
+                <Icon name='user' />
+                22 Friends
+              </a>
+            </Card.Content>
+          </Card>
+          <Card>
+            <Image src='/images/avatar/large/matthew.png' wrapped ui={false} />
+            <Card.Content>
+              <Card.Header>Matthew</Card.Header>
+              <Card.Meta>
+                <span className='date'>Joined in 2015</span>
+              </Card.Meta>
+              <Card.Description>
+                Matthew is a musician living in Nashville.
+              </Card.Description>
+            </Card.Content>
+            <Card.Content extra>
+              <a>
+                <Icon name='user' />
+                22 Friends
+              </a>
+            </Card.Content>
+          </Card>
+          <Card>
+            <Image src='/images/avatar/large/matthew.png' wrapped ui={false} />
+            <Card.Content>
+              <Card.Header>Matthew</Card.Header>
+              <Card.Meta>
+                <span className='date'>Joined in 2015</span>
+              </Card.Meta>
+              <Card.Description>
+                Matthew is a musician living in Nashville.
+              </Card.Description>
+            </Card.Content>
+            <Card.Content extra>
+              <a>
+                <Icon name='user' />
+                22 Friends
+              </a>
+            </Card.Content>
+          </Card>
+          <Card>
+            <Image src='/images/avatar/large/matthew.png' wrapped ui={false} />
+            <Card.Content>
+              <Card.Header>Matthew</Card.Header>
+              <Card.Meta>
+                <span className='date'>Joined in 2015</span>
+              </Card.Meta>
+              <Card.Description>
+                Matthew is a musician living in Nashville.
+              </Card.Description>
+            </Card.Content>
+            <Card.Content extra>
+              <a>
+                <Icon name='user' />
+                22 Friends
+              </a>
+            </Card.Content>
+          </Card>
+          <Card>
+            <Image src='/images/avatar/large/matthew.png' wrapped ui={false} />
+            <Card.Content>
+              <Card.Header>Matthew</Card.Header>
+              <Card.Meta>
+                <span className='date'>Joined in 2015</span>
+              </Card.Meta>
+              <Card.Description>
+                Matthew is a musician living in Nashville.
+              </Card.Description>
+            </Card.Content>
+            <Card.Content extra>
+              <a>
+                <Icon name='user' />
+                22 Friends
+              </a>
+            </Card.Content>
+          </Card>          
+        </Card.Group>
+      </Container>
   )
 }
 export default Content
