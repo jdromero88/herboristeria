d97a96643024789e9ffeca13df4cf816f5d9ec79
jdromero88 <jdromerov88@gmail.com>
2020-06-10 21:06:53 -0400

    Added some content to the footer

---
+++ src/container/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Container, Segment} from 'semantic-ui-react'
+
+const Footer = () => {
+  return(
+    <Container>
+      <Segment textAlign='center'>
+        <p>Santa Lucia © 2020</p>
+      </Segment>
+    </Container>
+  )
+}
+
+export default Footer
