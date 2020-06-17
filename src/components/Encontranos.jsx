5ddbc6e7dc740b32e6453824465d7815303026d1
jdromero88 <jdromerov88@gmail.com>
2020-06-16 20:05:28 -0400

    Added responsive iframe

--- src/components/Encontranos.jsx
+++ src/components/Encontranos.jsx
@@ -1,15 +1,22 @@
 import React from 'react'
-import { Container } from 'semantic-ui-react'
+import { Container, Embed } from 'semantic-ui-react'
 const Encontranos = () => {
   return(
-    <Container>
-      <h1>Nos encontras aqui</h1>
-      <iframe title="Herboristeria Santa Lucia"
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3558.542426811702!2d-57.02626088495563!3d-26.886275183138995!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x945a258d8ec11dc5%3A0xd12d474e363130b!2sHerboristeria%20Santa%20Lucia!5e0!3m2!1sen!2sus!4v1592344566205!5m2!1sen!2sus"
-        width="800" height="600" frameborder="0"
-        style={{border:'0'}}
-        allowfullscreen="" aria-hidden="false" tabindex="0">
-      </iframe>
+    <Container className='container-encontranos'>
+      <h1>Nos encontras aquí</h1>
+        <Embed
+          autoplay={true}
+          color='white'
+          iframe={{
+            allowFullScreen: true,
+            style: {
+              padding: 10,
+              border: 0,
+            },
+          }}
+          source='google maps'
+          url='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3558.542426811702!2d-57.02626088495563!3d-26.886275183138995!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x945a258d8ec11dc5%3A0xd12d474e363130b!2sHerboristeria%20Santa%20Lucia!5e0!3m2!1sen!2sus!4v1592344566205!5m2!1sen!2sus'
+        />
     </Container>
   )
 }
--- src/components/Nosotros.jsx
+++ src/components/Nosotros.jsx
@@ -4,7 +4,7 @@ import { Container } from 'semantic-ui-react'
 class Nosotros extends React.Component {
   render(){
     return(
-      <Container>
+      <Container fluid>
         <h1>Nosotros</h1>
         <p>Informacion acerca de la empresa va aqui.</p>
       </Container>
